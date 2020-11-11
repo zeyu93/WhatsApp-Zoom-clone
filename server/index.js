1e4d76999bea0c98092b0aa9a114e46d9b68474a
Zeyu Liu <zliu@ellisdon.com>
2020-11-10 21:57:52 -0500

    bug fix

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-
-const io = require("socket.io")(server);
+// const app = require("express")();
+// const server = require("http").createServer(app);
+var cors = require("cors");
+var io = require("socket.io")(5000, {
+  cors: {
+    origin: "*",
+  },
+});
 
 io.on("connection", (socket) => {
   //static id
+  console.log("connection");
   const id = socket.handshake.query.id;
   socket.join(id);
+
+  socket.on("send-message", ({ ids, text }) => {
+    ids.forEach((recipient) => {
+      const newids = ids.filter((r) => r != recipient);
+      newids.push(id);
+      socket.broadcast.to(recipient).emit("receive-message", {
+        ids: newids,
+        text,
+        sender: id,
+      });
+    });
+  });
 });
 
-server.listen(5000);
+// server.listen(5000);
